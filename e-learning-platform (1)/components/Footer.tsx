62d1324a827a6286a68a8a686a9a91388e5ca302
Nehemiah Nesanathan <144255177+NrGjoe846@users.noreply.github.com>
2025-01-28 10:01:40 +0530

    Delete e-learning-platform (1) directory

--- e-learning-platform (1)/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 p-4 text-center">
-      <p>&copy; 2023 TechQuest Academy. All rights reserved.</p>
-    </footer>
-  )
-}
-
